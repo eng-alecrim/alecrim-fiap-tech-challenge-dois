56e33c8ddb81eca61da434089a8aa2171d94cd17
eng-alecrim <lucasalecrimvieira@gmail.com>
2024-09-11 11:08:29 -0300

    versao "final"

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 
 from tech_challenge_dois.data_transformation import process_and_export_file
-from tech_challenge_dois.scheduler import time_to_run
+from tech_challenge_dois.scheduler import already_ran_today, time_to_run
 from tech_challenge_dois.scraping import download_file
 
 SLEEP_HOURS = 23
@@ -13,8 +13,9 @@ SLEEP_TIME_WAITING = 60
 def main() -> None:
     while True:
         if time_to_run():
-            path_downloaded_file = download_file()
-            process_and_export_file(filepath=path_downloaded_file)
+            if not already_ran_today():
+                path_downloaded_file = download_file()
+                process_and_export_file(filepath=path_downloaded_file)
             time.sleep(SLEEP_TIME_AFTER_RUN)
         time.sleep(SLEEP_TIME_WAITING)
 
--- tech_challenge_dois/scheduler.py
+++ tech_challenge_dois/scheduler.py
@@ -1,17 +1,18 @@
 from datetime import datetime, timedelta
+from pathlib import Path
 
 import pytz
 
 from tech_challenge_dois.settings import settings
 
 BRAZIL_TIME_ZONE = pytz.timezone("America/Sao_Paulo")
+TODAY = datetime.today().date()
 
 
 def time_to_run() -> bool:
-    today = datetime.today().date()
 
     scheduled_time = BRAZIL_TIME_ZONE.localize(
-        datetime.combine(today, datetime.min.time())
+        datetime.combine(TODAY, datetime.min.time())
         + timedelta(
             hours=settings.RUN_ON_HOUR, minutes=settings.RUN_ON_MINUTE, seconds=00
         )
@@ -22,3 +23,11 @@ def time_to_run() -> bool:
     delta = scheduled_time - current_time
 
     return delta <= timedelta(0)
+
+
+def already_ran_today() -> bool:
+    dir_data = Path(settings.PARQUET_FILES_DIR)
+    for file in dir_data.glob(f"*{TODAY.strftime("%d-%m-%y")}*.parquet"):
+        if file:
+            return True
+    return False
